bda1b5315bb4a7fdb9c981e5ff66db6071fbaefc
Prakash436 <102865908+Prakash436@users.noreply.github.com>
2022-08-05 23:17:41 +0530

    Add files via upload

--- deploy.py
+++ deploy.py
@@ -1,15 +1,20 @@
-from solcx import compile_standard, install_solc
 import json
 
 from web3 import Web3
 
+# In the video, we forget to `install_solc`
+# from solcx import compile_standard
+from solcx import compile_standard, install_solc
 import os
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
+# We add these two lines that we forgot from the video!
+print("Installing...")
 install_solc("0.6.0")
 
+# Solidity source code
 compiled_sol = compile_standard(
     {
         "language": "Solidity",
@@ -34,18 +39,24 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 ]["object"]
 
 # get abi
-abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
 
+# w3 = Web3(Web3.HTTPProvider(os.getenv("RINKEBY_RPC_URL")))
+# chain_id = 4
+#
+# For connecting to ganache
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 chain_id = 1337
+my_address = "0xfc1293FC0CC7E8C1Fd719d47f467ee6aE321AA25"
+private_key = "0x1708c9079768a23748d9194c557bbabb8c779964b87f81adf5cad75bfa35eebd"
 
-my_address = "0xA7aBFbA7cE4571dC962A993E18296e898c87a049"
-private_key = "0x77197c649b26006d0cc2d09da21d13157aed783d3675738441c90ea7438bc12f"
-
+# Create the contract in Python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
-
+# Get the latest transaction
 nonce = w3.eth.getTransactionCount(my_address)
-
+# Submit the transaction that deploys the contract
 transaction = SimpleStorage.constructor().buildTransaction(
     {
         "chainId": chain_id,
@@ -54,17 +65,22 @@ transaction = SimpleStorage.constructor().buildTransaction(
         "nonce": nonce,
     }
 )
-
+# Sign the transaction
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
-
+print("Deploying Contract!")
+# Send it!
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
 
+
+# Working with deployed Contracts
 simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
-
-
-greeting_transaction = simple_storage.functions.store(15).buildTransaction(
+print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
+val = int(input("Give value you want to store:"))
+greeting_transaction = simple_storage.functions.store(val).buildTransaction(
     {
         "chainId": chain_id,
         "gasPrice": w3.eth.gas_price,
@@ -72,15 +88,11 @@ greeting_transaction = simple_storage.functions.store(15).buildTransaction(
         "nonce": nonce + 1,
     }
 )
-
 signed_greeting_txn = w3.eth.account.sign_transaction(
     greeting_transaction, private_key=private_key
 )
-
 tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
-
+print("Updating stored Value...")
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
-
-simple_storage.functions.store(15).call()
 
 print(simple_storage.functions.retrieve().call())
