51ba6104038e9c90bb14575288d397f71cee7d01
Prakash436 <102865908+Prakash436@users.noreply.github.com>
2022-08-05 21:55:05 +0530

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,86 @@
+from solcx import compile_standard, install_solc
+import json
+
+from web3 import Web3
+
+import os
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+install_solc("0.6.0")
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 1337
+
+my_address = "0xA7aBFbA7cE4571dC962A993E18296e898c87a049"
+private_key = "0x77197c649b26006d0cc2d09da21d13157aed783d3675738441c90ea7438bc12f"
+
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+nonce = w3.eth.getTransactionCount(my_address)
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+
+greeting_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+
+signed_greeting_txn = w3.eth.account.sign_transaction(
+    greeting_transaction, private_key=private_key
+)
+
+tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
+
+simple_storage.functions.store(15).call()
+
+print(simple_storage.functions.retrieve().call())
